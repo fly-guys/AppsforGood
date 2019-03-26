47577d31470e9c4dd822a2d7812ef3b9589390b7
achintalapati <achintalapati@wpi.edu>
2019-03-26 10:48:13 -0400

    Added Spinner

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.appsforgood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation rotateAnimation;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=(ImageView)findViewById(R.id.Spinner);
+    }
+
+    public void rotateAnimation(View v) {
+        rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        imageView.startAnimation(rotateAnimation);
+
+
     }
 }
