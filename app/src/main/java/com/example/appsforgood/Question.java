664b50459ab449f06da27856b0b67a6368dad811
marissagardner <mjgardner@wpi.edu>
2019-03-27 12:50:28 -0400

    Added Question Class File

--- app/src/main/java/com/example/appsforgood/Question.java
+++ app/src/main/java/com/example/appsforgood/Question.java
@@ -1,9 +1,16 @@
 package com.example.appsforgood;
 
+import java.util.ArrayList;
+
 public class Question {
     //Data
+    private String question;
+    private ArrayList<String> answers;
 
     //Constructors
+    public Question(String questionText){
+        question = questionText;
+    }
 
     //Methods
 }
