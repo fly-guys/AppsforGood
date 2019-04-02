7fba9e6e849a3c594963e3fcee80b8dea359ee0c
achintalapati <achintalapati@wpi.edu>
2019-04-02 12:19:16 -0400

    Added Answers to TextView and Options Text to Button

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,14 +26,16 @@ public class MainActivity extends AppCompatActivity {
     Animation rotateAnimation;
     ImageView imageView;
 
-    private ArrayList<QData> questions = new ArrayList<>();
+    private ArrayList<QData> questionList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_questionview);
 
-        imageView=(ImageView)findViewById(R.id.Spinner);
+//        imageView=(ImageView)findViewById(R.id.Spinner);
+
+        readQData();
     }
 
     public void readQData(){
@@ -45,46 +48,57 @@ public class MainActivity extends AppCompatActivity {
             while((line = reader.readLine()) != null){
 
                 //Split by ","
-                String[] fields = line.split(",");
+                String[] fields = line.split("~");
 
-                questions.add(new QData(fields[0], fields[1], fields[2], fields[3], fields[4]));
+                questionList.add(new QData(fields[0], fields[1], fields[2], fields[3], fields[4]));
 
             }
         }
 
         catch(IOException io) {
 
-            Log.wtf("MainActivity", "ERROR raeding data on line " + line);
+            Log.wtf("MainActivity", "ERROR reading data on line " + line);
         }
 
-        String songStr = "";
+        String qStr = "";
+        String ans1Str = "";
+        String ans2Str = "";
+        String ans3Str = "";
+        String ans4Str = "";
+        String correctAnswer = ans1Str;
 
-        for(QData q : questions){
-            songStr += q.getQuestion() + " " + q.getAns1() + " " + q.getAns2() + " " + q.getAns3() + " " + q.getAns4() + "\n";
 
+        for(QData q : questionList){
+            qStr += q.getQuestion();
+            ans1Str += q.getAns1();
+            ans2Str += q.getAns2();
+            ans3Str += q.getAns3();
+            ans4Str += q.getAns4();
 
-            TextView songView = (TextView) findViewById(R.id.questionText);
+            ArrayList<String> answers = new ArrayList<>();
+            answers.add(ans1Str);
+            answers.add(ans2Str);
+            answers.add(ans3Str);
+            answers.add(ans4Str);
 
-            songView.setText(songStr);
+            TextView qView = (TextView) findViewById(R.id.questionText);
+            Button ans1Button = (Button) findViewById(R.id.option1);
+            Button ans2Button = (Button) findViewById(R.id.option2);
+            Button ans3Button = (Button) findViewById(R.id.option3);
+            Button ans4Button = (Button) findViewById(R.id.option4);
+
+            qView.setText(qStr);
+            ans1Button.setText(answers.get(randy.nextInt(3)));
+            answers.remove(ans1Button.getText());
+            ans2Button.setText(answers.get(randy.nextInt(2)));
+            answers.remove(ans2Button.getText());
+            ans3Button.setText(answers.get(randy.nextInt(1)));
+            answers.remove(ans3Button.getText());
+            ans4Button.setText(answers.get(0));
         }
     }
 
-    /**
-     * public void nextButtonAction(View v)
-     *     {
-     *         //Get Global Controller Class object (see application tag in AndroidManifest.xml)
-     *         final Controller aController = (Controller) getApplicationContext();
-     *
-     *         TextView textView = (TextView) findViewById(R.id.firstData);
-     *         String productName = textView.getText().toString();
-     *         System.out.println("PRODUCT NAME" + productName);
-     *         aController.getData().setProductName(productName);
-     *
-     *         Intent intent = new Intent(MainActivity.this, MainActivity2Activity.class);
-     *         startActivity(intent);
-     *
-     *     }
-     */
+
 
 
 //    public void rotateAnimation(View v) {
