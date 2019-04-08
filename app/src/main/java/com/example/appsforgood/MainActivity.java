4cec71ea4e1cc36a52e66b0546f68418922def43
achintalapati <achintalapati@wpi.edu>
2019-04-08 14:20:49 -0400

    Added Answers to TextView and Options Text to Button

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -29,12 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     String correctAnswer = "8";
 
-    private ArrayList<QData> questionList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_questionview);
+        setContentView(R.layout.activity_main);
 
 //        imageView=(ImageView)findViewById(R.id.Spinner);
 
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 //Split by ","
                 String[] fields = line.split("~");
 
-                questionList.add(new QData(fields[0], fields[1], fields[2], fields[3], fields[4]));
+                //questionList.add(new QData(fields[0], fields[1], fields[2], fields[3], fields[4]));
 
             }
         }
@@ -71,70 +71,70 @@ public class MainActivity extends AppCompatActivity {
         String correctAnswer = "";
 
 
-        TextView qView = (TextView) findViewById(R.id.questionText);
-
-        final Button ans1Button = (Button) findViewById(R.id.option1);
-        final Button ans2Button = (Button) findViewById(R.id.option2);
-        final Button ans3Button = (Button) findViewById(R.id.option3);
-        final Button ans4Button = (Button) findViewById(R.id.option4);
-
-        for(QData q : questionList){
-            qStr += q.getQuestion();
-            ans1Str += q.getAns1();
-            ans2Str += q.getAns2();
-            ans3Str += q.getAns3();
-            ans4Str += q.getAns4();
-
-//            correctAnswer = ans1Str;
-
-            ArrayList<String> answers = new ArrayList<>();
-            answers.add(ans1Str);
-            answers.add(ans2Str);
-            answers.add(ans3Str);
-            answers.add(ans4Str);
-
-            qView.setText(qStr);
-            ans1Button.setText(answers.get(randy.nextInt(3)));
-            answers.remove(ans1Button.getText());
-            ans2Button.setText(answers.get(randy.nextInt(2)));
-            answers.remove(ans2Button.getText());
-            ans3Button.setText(answers.get(randy.nextInt(1)));
-            answers.remove(ans3Button.getText());
-            ans4Button.setText(answers.get(0));
-
-            //New Comment
-        }
-
-//        if(ans1Button.isActivated() && ans1Button.getText().equals(correctAnswer)){
+//        TextView qView = (TextView) findViewById(R.id.questionText);
+//
+//        final Button ans1Button = (Button) findViewById(R.id.option1);
+//        final Button ans2Button = (Button) findViewById(R.id.option2);
+//        final Button ans3Button = (Button) findViewById(R.id.option3);
+//        final Button ans4Button = (Button) findViewById(R.id.option4);
+//
+//        for(QData q : questionList){
+//            qStr += q.getQuestion();
+//            ans1Str += q.getAns1();
+//            ans2Str += q.getAns2();
+//            ans3Str += q.getAns3();
+//            ans4Str += q.getAns4();
+//
+////            correctAnswer = ans1Str;
+//
+//            ArrayList<String> answers = new ArrayList<>();
+//            answers.add(ans1Str);
+//            answers.add(ans2Str);
+//            answers.add(ans3Str);
+//            answers.add(ans4Str);
+//
+//            qView.setText(qStr);
+//            ans1Button.setText(answers.get(randy.nextInt(3)));
+//            answers.remove(ans1Button.getText());
+//            ans2Button.setText(answers.get(randy.nextInt(2)));
+//            answers.remove(ans2Button.getText());
+//            ans3Button.setText(answers.get(randy.nextInt(1)));
+//            answers.remove(ans3Button.getText());
+//            ans4Button.setText(answers.get(0));
+//
+//            //New Comment
+//        }
+//
+////        if(ans1Button.isActivated() && ans1Button.getText().equals(correctAnswer)){
+////            Intent intent = new Intent(this, CorrectAnswer.class);
+////            startActivity(intent);
+////        }
+////        else if(ans2Button.isActivated() && ans2Button.getText().equals(correctAnswer)){
+////            Intent intent = new Intent(this, CorrectAnswer.class);
+////            startActivity(intent);
+////        }
+////        if(ans3Button.isActivated() && ans3Button.getText().equals(correctAnswer)){
+////            Intent intent = new Intent(this, CorrectAnswer.class);
+////            startActivity(intent);
+////        }
+////        if(ans4Button.isActivated() && ans4Button.getText().equals(correctAnswer)){
+////            Intent intent = new Intent(this, CorrectAnswer.class);
+////            startActivity(intent);
+////        }
+//
+//
+//
+//    }
+//
+//    public void checkCorrect(View v){
+//        Button b = (Button)v;
+//        String buttonText = b.getText().toString();
+//
+//        if(buttonText.equals(correctAnswer)){
 //            Intent intent = new Intent(this, CorrectAnswer.class);
 //            startActivity(intent);
 //        }
-//        else if(ans2Button.isActivated() && ans2Button.getText().equals(correctAnswer)){
-//            Intent intent = new Intent(this, CorrectAnswer.class);
-//            startActivity(intent);
-//        }
-//        if(ans3Button.isActivated() && ans3Button.getText().equals(correctAnswer)){
-//            Intent intent = new Intent(this, CorrectAnswer.class);
-//            startActivity(intent);
-//        }
-//        if(ans4Button.isActivated() && ans4Button.getText().equals(correctAnswer)){
-//            Intent intent = new Intent(this, CorrectAnswer.class);
-//            startActivity(intent);
-//        }
-
-
-
-    }
-
-    public void checkCorrect(View v){
-        Button b = (Button)v;
-        String buttonText = b.getText().toString();
-
-        if(buttonText.equals(correctAnswer)){
-            Intent intent = new Intent(this, CorrectAnswer.class);
-            startActivity(intent);
-        }
-
+//
     }
 
 
