7279c264bbcd91a2e150c84d0ab43708620f0cfe
achintalapati <achintalapati@wpi.edu>
2019-04-09 22:39:42 -0400

    Major Changes (Changed a lot of xml to accommodate activities)

--- app/src/main/java/com/example/appsforgood/Correct.java
+++ app/src/main/java/com/example/appsforgood/Correct.java
@@ -1,34 +1,13 @@
 package com.example.appsforgood;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
+import android.os.Bundle;
 
 public class Correct extends AppCompatActivity {
-
-    private final String TAG = "In MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_correctview);
-
-
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        String correct = bundle.getString("Correct");
-
-        TextView answerView = (TextView)findViewById(R.id.correctID);
-        answerView.setText(correct);
-
-        Log.d(TAG,correct);
-        String stupid = "stupid";
-
-
-//        imageView=(ImageView)findViewById(R.id.Spinner);
-
-
+        setContentView(R.layout.activity_correct);
     }
 }
---
+++ app/src/main/java/com/example/appsforgood/QuizActivity1.java
@@ -0,0 +1,78 @@
+package com.example.appsforgood;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class QuizActivity1 extends AppCompatActivity {
+
+    private ArrayList<Question> questionList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz1);
+
+        readQData();
+    }
+
+    public void readQData() {
+
+        InputStream is = getResources().openRawResource(R.raw.questions);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        String line = "";
+        try {
+            while ((line = reader.readLine()) != null) {
+
+                //Split by ","
+                String[] fields = line.split("~");
+
+                questionList.add(new Question(fields[0], fields[1], fields[2], fields[3], fields[4]));
+
+            }
+        } catch (IOException io) {
+
+            Log.wtf("MainActivity", "ERROR reading data on line " + line);
+        }
+
+        questionMaker(questionList);
+
+    }
+
+    public void questionMaker(ArrayList<Question> questionList){
+
+        TextView questionView = (TextView) findViewById(R.id.questionID);
+        Button option1 = (Button) findViewById(R.id.option1ID);
+        option1.setTag("Correct");
+        Button option2 = (Button) findViewById(R.id.option2ID);
+        Button option3 = (Button) findViewById(R.id.option3ID);
+        Button option4 = (Button) findViewById(R.id.option4ID);
+
+        questionView.setText(questionList.get(1).getQuestion());
+        option1.setText(questionList.get(1).getOption1());
+        option2.setText(questionList.get(1).getOption2());
+        option3.setText(questionList.get(1).getOption3());
+        option4.setText(questionList.get(1).getOption4());
+
+    }
+
+    boolean clicked = false;
+
+
+    public void onClick(View v){
+        Intent intent = new Intent(this,Correct.class);
+        intent.putExtra("Correct", "correct");
+        startActivity(intent);
+    }
+}
