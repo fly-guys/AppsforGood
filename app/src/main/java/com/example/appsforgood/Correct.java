4e8c8a741b166e775cbdf678a442c1fcdd75f8cd
achintalapati <achintalapati@wpi.edu>
2019-04-09 22:43:18 -0400

    Major Changes (Changed a lot of xml to accommodate activities)

--- app/src/main/java/com/example/appsforgood/Correct.java
+++ app/src/main/java/com/example/appsforgood/Correct.java
@@ -1,7 +1,9 @@
 package com.example.appsforgood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Correct extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Correct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_correct);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String answer = bundle.getString("Correct");
+
+        TextView answerView = (TextView)findViewById(R.id.correctID);
+        answerView.setText(answer);
     }
 }
