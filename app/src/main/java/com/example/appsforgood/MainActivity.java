9a08fa5ff0dbba887ffb1390103ca52bf8467179
marissagardner <mjgardner@wpi.edu>
2019-05-08 19:45:46 -0400

    Added instructions button on the homepage

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -3,25 +3,9 @@ package com.example.appsforgood;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Random;
-
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,LearnActivity.class);
         startActivity(intent);
     }
+
+    public void instructionsButton(View v){
+        Intent intent = new Intent(this, InstructionsActivity.class);
+        startActivity(intent);
+    }
+
+
 }
 
